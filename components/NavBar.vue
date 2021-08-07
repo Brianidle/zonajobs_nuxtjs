7790da57623b0c3a12dfb3424116d6f3fb355af6
Brianidle <ddbrayan10@hotmail.com>
2021-08-07 17:00:40 -0300

    New containers for the NavBar component

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,42 +1,45 @@
 <template>
-  <div class="navigationBar">
-    <a class="pageLogoButton">
-      <img
-        class="pageLogo"
-        src="https://imgzj.jobscdn.com/postulantes-assets/skins/zonajobs/commons/img/logo-zonajobs.svg"
-      />
-    </a>
+  <header>
+    <div class="navContainer">
+      <div class="navigationBar">
+        <a class="pageLogoButton">
+          <img
+            class="pageLogo"
+            src="https://imgzj.jobscdn.com/postulantes-assets/skins/zonajobs/commons/img/logo-zonajobs.svg"
+          />
+        </a>
 
-    <div class="navButtons">
-      <div class="optionButtonsContainer">
-        <a class="optionButton ">Posiciones Ejecutivas</a>
-        <a class="optionButton">Blog de Noticias</a>
-        <a class="optionButton">Soy Empresa</a>
-      </div>
-      <div class="specialButtonsContainer">
-        <a class="navBarPublishButton">Publicar Gratis</a>
-        <a class="navBarLogInButton">Ingresar</a>
+        <div class="navButtons">
+          <div class="optionButtonsContainer">
+            <a class="optionButton ">Posiciones Ejecutivas</a>
+            <a class="optionButton">Blog de Noticias</a>
+            <a class="optionButton">Soy Empresa</a>
+          </div>
+          <div class="specialButtonsContainer">
+            <a class="navBarPublishButton">Publicar Gratis</a>
+            <a class="navBarLogInButton">Ingresar</a>
+          </div>
+        </div>
+
+        <div class="navBarSeparator"></div>
+
+        <button class="dropdownButton">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="#000000"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
-
-    <div class="navBarSeparator"></div>
-
-    <button class="dropdownButton">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="#000000"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fillRule="evenodd"
-          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-        />
-      </svg>
-    </button>
-
-  </div>
+  </header>
 </template>
 
 <script>
@@ -44,6 +47,24 @@ export default {};
 </script>
 
 <style scoped>
+header {
+  z-index: 2;
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  background-color: white;
+}
+
+.navContainer {
+  height: 58px;
+  width: 1170px;
+  margin: 0px auto;
+
+  font-family: "Open Sans", sans-serif;
+  font-size: 14px;
+  font-weight: 600;
+}
+
 .navigationBar {
   display: inline-flex;
   flex-direction: row;
@@ -167,6 +188,12 @@ export default {};
 
   .navBarSeparator {
     display: block;
+  }
+}
+
+@media (max-width: 1250px) {
+  .navContainer {
+    width: 95%;
   }
 }
 </style>
