10ec10662a9087a638e621e7f849dafcabe2b552
Brianidle <ddbrayan10@hotmail.com>
2021-08-16 22:17:59 -0300

    Now when the user published a job ad the browser is redirected to the home page

--- pages/empresas/formularioGratis.vue
+++ pages/empresas/formularioGratis.vue
@@ -152,6 +152,8 @@ export default {
           urlCompanyLogo: this.urlCompanyLogo,
         }),
       });
+
+      this.$router.replace("/");
     },
     onClickCheckBox: function() {
       this.salary = "";
